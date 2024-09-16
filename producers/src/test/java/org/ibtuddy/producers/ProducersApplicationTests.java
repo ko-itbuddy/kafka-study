0141d311ee9e417351bd9395fd75148fb69815d2
나평근 <skvudrms54@gmail.com>
2024-09-16 12:29:06 +0900

    build: Kafka 프로젝트 생성및 멀티 모듈로 producer 추가

---
+++ producers/src/test/java/org/ibtuddy/producers/ProducersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ibtuddy.producers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/org/ibtuddy/kafkaproj01/KafkaProj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ibtuddy.kafkaproj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProj01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
