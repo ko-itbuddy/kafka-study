302b65be4298abafb45615fce475277da09acebd
나평근 <skvudrms54@gmail.com>
2024-09-17 20:24:02 +0900

    feat: Producer에서 키(Key) 값을 가지는 메시지 전송 구현

---
+++ producers/src/main/java/org/ibtuddy/producers/ProducerAsyncWithKey.java
@@ -0,0 +1,62 @@
+package org.ibtuddy.producers;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerAsyncWithKey {
+
+    public static final Logger logger = LoggerFactory.getLogger(ProducerAsyncWithKey.class);
+
+    public static void main(String[] args) {
+
+        String topicName = "multipart-topic";
+
+        //1. Producer 환경 설정
+        Properties props = new Properties();
+        //bootstrap.servers, broker 주소
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        // key.serializer.class
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class.getName());
+        // value.serializer.class
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class.getName());
+
+        //2. 1에서 설정한 환경 설섲ㅇ값을 반영하여 KafkaProducer 객체 생성
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        for(int seq=0; seq<20; seq++) {
+
+            //3. 토픽명과 메시지 값(ket,value)을 입력하여 보낼 메시지인 ProducerRecord 객체 생성
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName,String.valueOf(seq),
+                "hello-world " + seq);
+            //4. KafkaProducer 객체의 send 메소드를 호출하여 ProducerRecord 전송
+
+            kafkaProducer.send(producerRecord, (recordMetadata, exception) -> {
+                if (exception == null) {
+                    logger.info("\n ##### record metadata received  #####\n" +
+                        "partition:" + recordMetadata.partition() + "\n" +
+                        "offset:" + recordMetadata.offset() + "\n" +
+                        "timestamp:" + recordMetadata.timestamp()
+                    );
+                } else {
+                    logger.error("exception error form broker " + exception.getMessage());
+                }
+            });
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        kafkaProducer.close();
+
+    }
+}
